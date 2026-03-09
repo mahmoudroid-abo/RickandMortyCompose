aa2403000161896930dcf6ae723e4a801fa44074
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-09 02:11:42 +0200

    add Ktor dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -51,4 +51,10 @@ dependencies {
     ksp(libs.hilt.compiler)
 
     implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
 }
