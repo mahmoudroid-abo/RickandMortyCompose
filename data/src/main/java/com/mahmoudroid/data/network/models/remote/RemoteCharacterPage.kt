faee31877508d356a1af0a5c4d5a414d941a239b
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-17 23:06:13 +0200

    add RemoteCharacterPage

---
+++ data/src/main/java/com/mahmoudroid/data/network/models/remote/RemoteCharacterPage.kt
@@ -0,0 +1,31 @@
+package com.mahmoudroid.data.network.models.remote
+
+
+import com.mahmoudroid.data.network.models.domain.CharacterPage
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RemoteCharacterPage(
+    val info: Info,
+    val results: List<RemoteCharacter>
+) {
+    @Serializable
+    data class Info(
+        val count: Int,
+        val pages: Int,
+        val next: String?,
+        val prev: String?
+    )
+}
+
+fun RemoteCharacterPage.toDomainCharacterPage(): CharacterPage {
+    return CharacterPage(
+        info = CharacterPage.Info(
+            count = info.count,
+            pages = info.pages,
+            next = info.next,
+            prev = info.next
+        ),
+        characters = results.map { it.toDomainCharacter() }
+    )
+}
