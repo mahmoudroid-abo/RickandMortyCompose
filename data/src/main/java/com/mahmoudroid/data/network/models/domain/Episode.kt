b8794b2107c9e296a375843057fb4ac2accfa6a9
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-17 23:05:45 +0200

    add Episode

---
+++ data/src/main/java/com/mahmoudroid/data/network/models/domain/Episode.kt
@@ -0,0 +1,11 @@
+package com.mahmoudroid.data.network.models.domain
+
+
+data class Episode(
+    val id: Int,
+    val name: String,
+    val seasonNumber: Int,
+    val episodeNumber: Int,
+    val airDate: String,
+    val characterIdsInEpisode: List<Int>
+)
