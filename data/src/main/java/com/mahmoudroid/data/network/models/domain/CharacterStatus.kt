2dd5c59363c0343e80808e1f91052ce703ce9cc1
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-17 23:05:32 +0200

    add CharacterStatus

---
+++ data/src/main/java/com/mahmoudroid/data/network/models/domain/CharacterStatus.kt
@@ -0,0 +1,8 @@
+package com.mahmoudroid.data.network.models.domain
+
+
+sealed class CharacterStatus(val displayName: String) {
+    object Alive: CharacterStatus("Alive")
+    object Dead: CharacterStatus("Dead")
+    object Unknown: CharacterStatus("Unknown")
+}
